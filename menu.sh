d61c05ecde9d3da2fcdfa520f524c2dc3992ace8
demianrey <demianrey@users.noreply.github.com>
2026-07-24 09:49:40 -0600

    Corregir opcion 11 (warp_unlock/TG push): no pasar -S al script externo
    El script externo fscarmen/warp_unlock solo tiene textos en ingles/chino (arrays E[]/C[]). Nuestro fork guarda "S" en /etc/wireguard/language al elegir espanol, y unlock.sh lee ese mismo archivo para su propio idioma. Al recibir L=S, su funcion text() hace un nameref a un array "S" inexistente y devuelve string vacio para TODOS los mensajes, por lo que el script no mostraba nada y salia sin avisar. Ahora se le pasa siempre -E o -C explicitamente.

--- menu.sh
+++ menu.sh
@@ -2852,7 +2852,7 @@ menu_setting() {
 
   ACTION[4]() { OPTION=o; onoff; }
   ACTION[5]() { client_install; }; ACTION[6]() { change_ip; }; ACTION[7]() { uninstall; }; ACTION[8]() { update; }; ACTION[9]() { bbrInstall; }; ACTION[10]() { ver; };
-  ACTION[11]() { bash <(curl -sSL https://gitlab.com/fscarmen/warp_unlock/-/raw/main/unlock.sh) -$L; };
+  ACTION[11]() { bash <(curl -sSL https://gitlab.com/fscarmen/warp_unlock/-/raw/main/unlock.sh) -"$([ "$L" = C ] && echo C || echo E)"; };
   ACTION[12]() { IS_ANEMONE=is_anemone ;install; };
   ACTION[13]() { IS_PUFFERFFISH=is_pufferffish; install; };
   ACTION[14]() { IS_LUBAN=is_luban; client_install; };
